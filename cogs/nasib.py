23f7d40182742f91e9825e50140cb27cdce3717a
Your Name <you@example.com>
2020-05-30 21:07:19 +0700

    yeah5

---
+++ cogs/nasib.py
@@ -0,0 +1,32 @@
+import discord
+from discord.ext import commands
+import random
+
+class Nasib(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('cogs nasib loaded')
+
+    @commands.command()
+    async def nasib(self, ctx, *, question):
+        jawaban = ['Iya',
+                   'Sudah pasti',
+                   'Tentu saja',
+                   'Sudah mutlak',
+                   'Tidak mungkin',
+                   'Mustahil',
+                   'Ngaco, ya kagak lah',
+                   'Tidak',
+                   'g',
+                   'Bisa jadi'
+                   'Mungkin saja',
+                   'Kayaknya',
+                   'Gak tau juga']
+
+        await ctx.send(f'{random.choice(responses)}')
+
+def setup(client):
+    client.add_cog(Nasib(client))
