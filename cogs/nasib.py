6012275dbb996a7969ba4a1a768da22b44f36e9e
Your Name <you@example.com>
2020-05-31 10:12:43 +0700

    yea

--- cogs/nasib.py
+++ cogs/nasib.py
@@ -32,7 +32,7 @@ class Nasib(commands.Cog):
 
     @commands.command()
     async def lotre(self, ctx):
-        await ctx.send(f'Nomor lotre {message.author} adalah ```{random.randint(100000000, 999999999)}```')
+        await ctx.send(f'Nomor lotre anda adalah ```{random.randint(100000000, 999999999)}```')
 
 def setup(client):
     client.add_cog(Nasib(client))
