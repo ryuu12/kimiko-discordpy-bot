e12aaff03d6fa7704a95ab2e0d3a67322668c85c
Your Name <you@example.com>
2020-06-01 18:50:59 +0700

    report cogs

--- cogs/report.py
+++ cogs/report.py
@@ -13,7 +13,7 @@ class Report(commands.Cog):
     @commands.command()
     @commands.has_any_role(481671645808033809)
     async def report(self, ctx, *, message):
-        await ctx.send(discord.Object(id='495597599508922378'), "{}".format(message))
+        await ctx.send(discord.Object(id=495597599508922378), "{}".format(message))
     
 def setup(client):
     client.add_cog(Report(client))
