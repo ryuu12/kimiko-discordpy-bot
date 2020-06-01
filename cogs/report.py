f5a22c938352c1a1943600f1d3c213c3fdea7cd6
Your Name <you@example.com>
2020-06-01 19:36:18 +0700

    report cogs

--- cogs/report.py
+++ cogs/report.py
@@ -13,7 +13,7 @@ class Report(commands.Cog):
     @commands.command()
     @commands.has_any_role(481671645808033809)
     async def report(self, ctx):
-        channel = client.get_channel(495597599508922378)
+        channel = client.get_channel(549717043671334913)
         await ctx.send(channel, 'hello')
     
 def setup(client):
