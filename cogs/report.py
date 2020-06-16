f27deaeb6fc92085ba192bd4354d91d7994d1e44
Your Name <you@example.com>
2020-06-16 16:55:50 +0700

    tambah tanda bintang

--- cogs/report.py
+++ cogs/report.py
@@ -14,15 +14,15 @@ class Report(commands.Cog):
     async def report(self, ctx, member: discord.Member, *, arg):
         channel = self.client.get_channel(495597599508922378)
         author = ctx.message.author
-        await ctx.send(f'{member} sudah dilaporkan.')
-        await channel.send(f'{member} dilaporkan karena {arg} oleh {author}')
+        await ctx.send(f'**{member}** sudah dilaporkan.')
+        await channel.send(f'**{member}** dilaporkan karena {arg} oleh **{author}**')
 
     @commands.command()
     @commands.has_any_role(481671645808033809)
     async def warn(self, ctx, member: discord.Member, *, arg):
         channel = self.client.get_channel(495597599508922378)
-        await ctx.send(f'{member} diberikan peringatan. Alasan: {arg}')
-        await channel.send(f'{member} diberikan peringatan. Alasan: {arg}')
+        await ctx.send(f'**{member}** diberikan **peringatan** karena {arg}')
+        await channel.send(f'**{member}** diberikan **peringatan** karena {arg}')
 
 
 def setup(client):
